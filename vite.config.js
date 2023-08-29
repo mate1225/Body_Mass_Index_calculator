72dd314cb03caa5edaff80f80ee8b0efb8ee309d
mate1225 <mate981225@gmail.com>
2023-08-29 16:55:07 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Body_Mass_Index_calculator/",
+  base: "/Body_Mass_Index_calculator.git/",
 });
