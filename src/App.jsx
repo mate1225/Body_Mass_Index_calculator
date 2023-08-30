7e411f4f711b179d196eacf3c6437265254f4028
mate1225 <mate981225@gmail.com>
2023-08-30 15:26:11 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,35 @@ import IconPregnancy from "./assets/images/icon-pregnancy.svg";
 import IconRace from "./assets/images/icon-race.svg";
 //meg kell még csinálni a tailwind css-nek a prettier setup-ját!!!!
 
+function Footer() {
+  return (
+    <>
+      <footer className="flex flex-wrap items-center  justify-center gap-5 bg-bgColor p-4 xl:text-lg">
+        <div className="flex ">
+          <p className="text-Dark_Electric_Blue">Challenge by:</p>
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            className=" cursor-pointer  text-Blue"
+          >
+            Frontend Mentor
+          </a>
+        </div>
+        <div className="flex">
+          <p className=" text-Dark_Electric_Blue">Coded by:</p>
+          <a
+            href="https://github.com/mate1225/mate1225.github.io"
+            target="_blank"
+            className=" cursor-pointer  text-Blue"
+          >
+            Mészáros Máté
+          </a>
+        </div>
+      </footer>
+    </>
+  );
+}
+
 function App() {
   return (
     <div>
@@ -116,6 +145,7 @@ function App() {
           GP or practice nurse."
         />
       </section>
+      <Footer />
     </div>
   );
 }
